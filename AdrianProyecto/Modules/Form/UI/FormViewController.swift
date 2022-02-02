bbf015a5f38684c4791484c36e4742b17c8f02b2
adrian <adrian@iraizos.me>
2022-02-02 20:53:44 +0100

    texfields a form

--- AdrianProyecto/Modules/Form/UI/FormViewController.swift
+++ AdrianProyecto/Modules/Form/UI/FormViewController.swift
@@ -8,10 +8,27 @@
 import UIKit
 
 class FormViewController: UIViewController {
-
+    
+    //MARK: Oulets
+    
+    @IBOutlet weak var cifTextField: UITextField!
+    @IBOutlet weak var companyNameTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var countryCheckDigitsTextField: UITextField!
+    @IBOutlet weak var bankTextField: UITextField!
+    @IBOutlet weak var officeTextField: UITextField!
+    @IBOutlet weak var accountCheckDigitsTextField: UITextField!
+    @IBOutlet weak var accountTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField! {
+        didSet {
+            emailTextField.placeholder = NSLocalizedString("user_form_email_text_field_placeholder", comment: "")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 }
