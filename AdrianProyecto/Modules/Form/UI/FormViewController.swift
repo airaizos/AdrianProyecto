41c24458bd3fa544c94450688bf8cc581831193c
adrian <adrian@iraizos.me>
2022-02-02 21:27:15 +0100

    añadiendo idioma

--- AdrianProyecto/Modules/Form/UI/FormViewController.swift
+++ AdrianProyecto/Modules/Form/UI/FormViewController.swift
@@ -14,12 +14,37 @@ class FormViewController: UIViewController {
     @IBOutlet weak var cifTextField: UITextField!
     @IBOutlet weak var companyNameTextField: UITextField!
     @IBOutlet weak var addressTextField: UITextField!
-    @IBOutlet weak var countryCheckDigitsTextField: UITextField!
-    @IBOutlet weak var bankTextField: UITextField!
-    @IBOutlet weak var officeTextField: UITextField!
-    @IBOutlet weak var accountCheckDigitsTextField: UITextField!
-    @IBOutlet weak var accountTextField: UITextField!
-    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var countryCheckDigitsTextField: UITextField!   {
+        didSet {
+            bankTextField.placeholder = NSLocalizedString("user_form_country_check_digits_text_field_placeholder", comment: "")
+        }
+    }
+    @IBOutlet weak var bankTextField: UITextField!  {
+        didSet {
+            bankTextField.placeholder = NSLocalizedString("user_form_bank_text_field_placeholder", comment: "")
+        }
+    }
+    @IBOutlet weak var officeTextField: UITextField! {
+        didSet {
+            officeTextField.placeholder = NSLocalizedString("user_form_ofice_text_field_placeholder", comment: "")
+        }
+    }
+    @IBOutlet weak var accountCheckDigitsTextField: UITextField!  {
+        didSet {
+            accountCheckDigitsTextField.placeholder = NSLocalizedString("user_form_account_check_digits_text_field_placeholder", comment: "")
+        }
+    }
+    @IBOutlet weak var accountTextField: UITextField! {
+        didSet {
+            accountTextField.placeholder = NSLocalizedString("user_form_account_text_field_placeholder", comment: "")
+        }
+    }
+    
+    @IBOutlet weak var phoneTextField:  UITextField!  {
+        didSet {
+            phoneTextField.placeholder = NSLocalizedString("user_form_phone_text_field_placeholder", comment: "")
+        }
+    }
     
     @IBOutlet weak var emailTextField: UITextField! {
         didSet {
